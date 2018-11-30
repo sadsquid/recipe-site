6783cfa81d854e4887e1985b1da21cfd5fb5266b
Cuong Duong <cuong.duong@target.com>
2018-11-30 12:33:16 -0600

    Updating app

--- src/containers/pageContainers/About/index.js
+++ src/containers/pageContainers/About/index.js
@@ -5,10 +5,15 @@ const About = () => (
   <div className="About">
     <div className="About-content">
       <p className="About-quote">
-        "We should have a site where we can show people pictures of us and food and stuff!"
+        " We should have a site where we can show people pictures of{' '}
+        <span className="About-bold">us</span>
+        {' and '}
+        <span className="About-bold">food</span>
+        {' and '}
+        <span className="About-bold">stuff</span> "
       </p>
-      <p>todo: add pics of us here</p>
-      <p className="About-quote" />
+      <p className="About-todo">todo: add pics of us here...</p>
+      {/* <p className="About-quote" /> */}
 
       <p className="About-maintext"> </p>
     </div>
